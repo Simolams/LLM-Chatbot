caed24ad121fbb104192aae9cb5846c4d62bb614
Simolams <mohamedtaalim08@gmail.com>
2023-09-18 21:22:06 +0200

    add importing libraries

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from htmlTemplates import bot_template, user_template, css
+from langchain.llms import HuggingFaceHub
+import os
+from nltk.tokenize import word_tokenize

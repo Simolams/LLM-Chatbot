453322f28affa253599b1ccbf74fe3bb44fd3d3a
Simolams <mohamedtaalim08@gmail.com>
2023-09-18 21:31:05 +0200

    add the acces to the hugging face api

--- app.py
+++ app.py
@@ -10,3 +10,19 @@ from htmlTemplates import bot_template, user_template, css
 from langchain.llms import HuggingFaceHub
 import os
 from nltk.tokenize import word_tokenize
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+# Access the HuggingFace API
+REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
+
+def get_pdf_text(pdf_files):
+    
+    text = ""
+    for pdf_file in pdf_files:
+        reader = PdfReader(pdf_file)
+        for page in reader.pages:
+            text += page.extract_text()
+    return text

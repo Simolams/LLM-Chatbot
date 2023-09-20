72865c470fb8d06884849e908a765138b0b51909
Simolams <mohamedtaalim08@gmail.com>
2023-09-20 19:44:26 +0200

    add get_conversation_chain,handle_user_input

--- app.py
+++ app.py
@@ -63,3 +63,30 @@ def get_vector_store(text_chunks):
     vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
     
     return vectorstore
+
+def get_conversation_chain(vector_store):
+    
+
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512},huggingfacehub_api_token = "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")
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = vector_store.as_retriever(),
+        memory = memory
+    )
+
+    return conversation_chain
+
+def handle_user_input(question):
+
+    response = st.session_state.conversation({'question':question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+
